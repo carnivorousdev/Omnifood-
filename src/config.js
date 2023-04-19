0a7937f1cd763303a839913f567d5c768e5c90d0
Biswa <biswaranjan.s@techvibrant.com>
2023-04-20 00:52:35 +0530

    Remove config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
 
 export const version = '3.5.0';
 export const navbarBreakPoint = 'xl'; // Vertical navbar breakpoint
@@ -21,9 +22,25 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+
 // Initialize Firebase
 const firebaseAppConfig = initializeApp(firebaseConfig);
 
 export const firestoreAuth = getAuth(firebaseAppConfig)
+
+
+const fireStoreConfig = {
+  apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRESTORE_APP_ID
+};
+
+// Initialize FireStore
+const fireBaseStoreConfig = initializeApp(fireStoreConfig);
+
+export const OmnifoodServer = getFirestore(fireBaseStoreConfig)
 
 export default { version, navbarBreakPoint, topNavbarBreakpoint, settings };
